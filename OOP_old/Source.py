9e2158092d6d9f351cfa4de38e6295db90eb8007
taceroc <taceroc@unal.edu.co>
2024-07-11 16:43:28 -0400

    Remove the now ignored directory OOP_old

--- OOP_old/Source.py
+++
@@ -1,9 +0,0 @@
-class Source:
-    def __init__(self, dt0, d, Flmax):
-        """
-            dt0: duration source in y
-            d: position observer -source
-        """
-        self.dt0 = dt0
-        self.d = d
-        self.Flmax = Flmax
