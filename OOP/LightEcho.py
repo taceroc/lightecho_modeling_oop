fix units

OOP/LightEcho.py
@@ -495,8 +495,8 @@ class LESheetDust(LE):
                 np.linspace(y_lim_min, y_lim_max, 1000))
         print(self.r_le_in.shape, self.x_projected[0,0,:].shape)
         def interpolation_radiis():
-            f_IN = interpolate.NearestNDInterpolator(list(zip(self.x_projected[0,0,:], self.y_projected[0,0,:])), self.r_le_in)
-            f_OUT = interpolate.NearestNDInterpolator(list(zip(self.x_projected[0,1,:], self.y_projected[0,1,:])), self.r_le_out)
+            f_IN = interpolate.NearestNDInterpolator(list(zip(self.x_projected[0,1,:], self.y_projected[0,1,:])), self.r_le_in)
+            f_OUT = interpolate.NearestNDInterpolator(list(zip(self.x_projected[0,0,:], self.y_projected[0,0,:])), self.r_le_out)
             return f_IN, f_OUT
         
         z_all = -(self.D/self.F) - (self.A * x_all / self.F) - (self.B * y_all / self.F)
@@ -612,16 +612,16 @@ class LESheetDust(LE):
             y=self.y_inter_values,
             z=self.z_inter_values,
             opacity=1))
-        xp, yp = np.meshgrid(self.x_inter_values, self.y_inter_values)
-        figs.add_trace(go.Surface(
-            x=xp,
-            y=yp,
-            z=-(self.D/self.F) - (self.A * xp / self.F) - (self.B * yp / self.F),
-            opacity=1))
+        # xp, yp = np.meshgrid(self.x_inter_values, self.y_inter_values)
+        # figs.add_trace(go.Surface(
+        #     x=xp,
+        #     y=yp,
+        #     z=-(self.D/self.F) - (self.A * xp / self.F) - (self.B * yp / self.F),
+        #     opacity=1))
         
         figs.add_trace(go.Scatter3d(
             x = [self.x_min, self.x_max, self.x_max, self.x_min, self.x_min, self.x_max, self.x_max, self.x_min],
             y = [self.y_min, self.y_min, self.y_max, self.y_max, self.y_max, self.y_max, self.y_min, self.y_min,],
             z = [self.z_min, self.z_min, self.z_min, self.z_min, self.z_max, self.z_max, self.z_max, self.z_max],
-              showlegend = False))
+              showlegend = True))
         figs.show()

OOP/SimulateLEFixPlane.py
@@ -222,10 +222,10 @@ def run(file_name, args):
         az = parameters['plane_coefficients'][2] 
         z0ly = parameters['plane_coefficients'][3] * fc.pctoly
         params = [a, ay, az, -z0ly]
-        x0 = parameters['dust_position'][0]
-        y0 = parameters['dust_position'][1]
-        z0 = (z0ly - a*x0 - ay*y0) / az if az != 0 else 0
-        dust_position = np.array([x0, y0, z0]) * fc.pctoly
+        x0 = parameters['dust_position'][0] *fc.pctoly
+        y0 = parameters['dust_position'][1] *fc.pctoly
+        z0 = (z0ly - (a*x0) - (ay*y0)) / (az) if az != 0 else 0
+        dust_position = np.array([x0, y0, z0])
     else:
         print("known")
         x0 = parameters['dust_position'][0]